5cc003f4eeb2a3f27caded905d71780f6fb7b1a2
Mohammed Dawood <mahmedtony73@gmail.com>
2022-02-23 14:52:13 +0200

    Update App.js
    Convert .then to async/await

--- src/App.js
+++ src/App.js
@@ -6,26 +6,23 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        // transofrm data to the names that i use in the app
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
 
-        console.log(data.results);
-        // set Movies State
-        setMovies(transformedMovies);
-      });
+    // transofrm data to the names that i use in the app
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+
+    // console.log(data.results);
+    // set Movies State
+    setMovies(transformedMovies);
   }
 
   return (
