8134e962188cb0bcecf5c229bc15a436d7863c2c
Mohammed Dawood <mahmedtony73@gmail.com>
2022-02-24 15:29:58 +0200

    Sending a POST Request
    Using fire base real time data base to provide the server and data base layer to use.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback, useEffect } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -13,7 +14,9 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-http-c5e4d-default-rtdb.europe-west1.firebasedatabase.app/movies.json"
+      );
 
       // check before setting(parse) data
       if (!response.ok) {
@@ -21,20 +24,34 @@ function App() {
       }
 
       const data = await response.json();
+      console.log(data);
+
+      // parse movies data
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      console.log(loadedMovies);
 
       // transofrm data to the names that i use in the app
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
 
       // console.log(data.results);
+
       // set Movies State
-      setMovies(transformedMovies);
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -42,6 +59,7 @@ function App() {
     setIsLoading(false);
   }, []);
 
+  // call fetchMoviesHandler on useEffect Hook (componentDidMount)
   useEffect(() => {
     fetchMoviesHandler();
 
@@ -49,6 +67,25 @@ function App() {
     //   second
     // }
   }, [fetchMoviesHandler]);
+
+  // add new movie to the movies array
+  async function addMovieHandler(movie) {
+    // console.log(movie);
+    // make the request
+    const response = await fetch(
+      "https://react-http-c5e4d-default-rtdb.europe-west1.firebasedatabase.app/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    // Data
+    const data = await response.json();
+    console.log(data);
+  }
 
   //replace JSX with content
   let content = <h1>Found No Movies...</h1>;
@@ -67,6 +104,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
